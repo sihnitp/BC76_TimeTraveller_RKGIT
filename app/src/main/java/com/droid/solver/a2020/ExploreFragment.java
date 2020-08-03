0093f93b4fb87955595e9153497e958f3bd6cf91
suprabhatkr <jnvsuprabh@gmail.com>
2020-08-03 10:35:13 +0530

    City Search is added to the the main activity also.

--- app/src/main/java/com/droid/solver/a2020/ExploreFragment.java
+++ app/src/main/java/com/droid/solver/a2020/ExploreFragment.java
@@ -19,6 +19,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.droid.solver.a2020.explore.ExploreActivity;
@@ -35,14 +36,21 @@ public class ExploreFragment extends Fragment implements  View.OnClickListener{
     private RecyclerView recyclerView;
     private MaterialButton button;
     private FusedLocationProviderClient locationProviderClient;
-
+    private String[] state;
+    private EditText inputText;
     public static ExploreFragment getInstance(){
         return new ExploreFragment();
     }
 
     public ExploreFragment() {
 
-
+        state=new String[]{
+                "andhra pradesh","arunachal pradesh","assam","bihar","chhattisgarh","goa","gujarat","haryana","himachal pradesh",
+                "jammu and kashmir","jharkhand","karnataka","kerela","madhya pradesh","maharashtra","manipur","meghalaya","mizoram",
+                "nagaland","odisha","punjab","rajasthan","sikkim","tamil nadu","telangana","tripura","uttar pradesh","uttarakhand",
+                "west bengal","andaman and nicobar islands","chandigarh","dadar and nagar haveli","daman and diu","lakshadweep",
+                "delhi","puducherry"
+        };
     }
 
     @Override
@@ -62,6 +70,7 @@ public class ExploreFragment extends Fragment implements  View.OnClickListener{
         button=view.findViewById(R.id.materialButton);
         locationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
         button.setOnClickListener(this);
+        inputText=view.findViewById(R.id.searchInput);
         return view;
     }
 
@@ -146,10 +155,30 @@ public class ExploreFragment extends Fragment implements  View.OnClickListener{
         });
 
     }
-
+    public void searchItem(){
+        String inputString = inputText.getText().toString();
+        inputString = inputString.toLowerCase();
+        String[] state_city = findSearchedCity(inputString);
+        //Log.i("hi bro", "searchItem:"+state_city[1]+inputString);
+        int state_index;
+        if (state_city[0]=="none"){
+            for (int i=0;i<state.length;i++){
+                if (inputString==state[i]){
+                    state_index=i;
+                }
+            }
+        }
+        else{
+            Intent intent=new Intent(getActivity(), ExploreActivity.class);
+            intent.putExtra("state", state_city[0]);
+            intent.putExtra("city", state_city[1]);
+            startActivity(intent);
+        }
+    }
     public void onClick(View view){
         if(view.getId()==R.id.materialButton){
-            requestLocationRequest();
+            //requestLocationRequest();
+            searchItem();
         }
     }
 
@@ -169,6 +198,58 @@ public class ExploreFragment extends Fragment implements  View.OnClickListener{
             }
         }
         return new String[] {CONSTANT.stateArr[minIndex],CONSTANT.cityArr[minIndex]};
+    }
+
+    private String [] findSearchedCity(String city_name){
+
+        String []cityArr=new String[]{"amaravati", "visakhapatnam", "vijayawada", "tirupati", "guntur", "port blair", "neil island",
+                "ross island", "diglipur", "mayabunder", "tawang", "itanagar", "Zero", "bomdila", "pasighat", "guwahati", "silchar",
+                "dibrugarh", "jorhat", "nagaon", "patna", "gaya", "bhagalpur", "nalanda", "darbhanga", "sitamarhi", "madhubani",
+                "chhapra", "buxar", "ara", "muzaffarpur", "saharsa", "begusarai", "ghaziabad", "chandigarh", "raipur", "bilaspur",
+                "bastar", "silavasa", "daman", "diu", "delhi", "panaji", "ponda", "ahmedabad", "surat", "vadodra", "rajkot", "gandhinagar",
+                "porbandar", "karnal", "hisar", "rohtak", "sonipat", "panipat", "shimla", "jammu", "kashmir", "ranchi", "jamshedpur", "deoghar",
+                "bokaro", "bengaluru", "hampi", "mangalore", "ooty", "thiruvananthapuram", "kochi", "kavaratti", "bhopal", "indore", "gwalior",
+                "ujjain", "mumbai", "auranagabad", "nagpur", "nashik", "khandala", "imphal", "shillong", "aizwal", "kohima", "dimapur", "mirzapur",
+                "bhubaneswar", "cuttack", "rourkela", "pudducherry", "mahe", "karaikal", "yanam", "amritsar", "patiala", "jalandhar", "ludhiana",
+                "jaipur", "jodhpur", "udaipur", "jaisalmer", "ajmer", "bikaner", "kota", "pushkar", "alwar", "chittorgarh", "gangtok", "chennai",
+                "madurai", "coimbatore", "tiruchirappali", "vellore", "hyderabad", "warangal", "lucknow", "prayagraj", "kanpur", "agra", "varanasi",
+                "aligarh", "bareilly", "jhansi", "mathura", "ayodhya", "jaunpur", "firozabad", "fatehpur sikri", "gorakhpur", "vrindavan", "dehradun",
+                "haridwar", "kashipur", "roorkee", "kolkata", "asansol", "siliguri", "durgapur", "haldia", "darjeeling", "malda", "khargpur",
+                "jalpaiguri", "birbhum", "bishnupur", "howrah", "bardhaman", "chandannagar"};
+
+        String [] stateArr=new String []{"andhra pradesh", "andhra pradesh", "andhra pradesh", "andhra pradesh", "andhra pradesh",
+                "andaman and nicobar islands", "andaman and nicobar islands", "andaman and nicobar islands", "andaman and nicobar islands",
+                "andaman and nicobar islands", "arunachal pradesh", "arunachal pradesh", "arunachal pradesh", "arunachal pradesh",
+                "arunachal pradesh", "assam", "assam", "assam", "assam", "assam", "bihar", "bihar", "bihar", "bihar", "bihar", "bihar",
+                "bihar", "bihar", "bihar", "bihar", "bihar", "bihar", "bihar", "uttar pradesh", "chandigarh", "chhattisgarh", "chhattisgarh",
+                "chhattisgarh", "dadra and nagar haveli", "daman and diu", "daman and diu", "delhi", "goa", "goa", "gujrat", "gujrat",
+                "gujrat", "gujrat", "gujrat", "gujrat", "haryana", "haryana", "haryana", "haryana", "haryana", "himachal pradesh",
+                "jammu and kashmir", "jammu and kashmir", "jharkhand", "jharkhand", "jharkhand", "jharkhand", "karnataka", "karnataka",
+                "karnataka", "karnataka", "kerla", "kerla", "lakshadweep", "madhya pradesh", "madhya pradesh", "madhya pradesh",
+                "madhya pradesh", "maharashtra", "maharashtra", "maharashtra", "maharashtra", "maharashtra", "manipur", "meghalaya",
+                "mizoram", "nagaland", "nagaland", "uttar pradesh", "odisha", "odisha", "odisha", "pondicherry", "pondicherry", "pondicherry",
+                "pondicherry", "punjab", "punjab", "punjab", "punjab", "rajsthan", "rajsthan", "rajsthan", "rajsthan", "rajsthan", "rajsthan",
+                "rajsthan", "rajsthan", "rajasthan", "rajasthan", "sikkim", "tamil nadu", "tamil nadu", "tamil nadu", "tamil nadu", "tamil nadu",
+                "telangana", "telangana", "uttar pradesh", "uttar pradesh", "uttar pradesh", "uttar pradesh", "uttar pradesh", "uttar pradesh",
+                "uttar pradesh", "uttar pradesh", "uttar pradesh", "uttar pradesh", "uttar pradesh", "uttar pradesh", "uttar pradesh",
+                "uttar pradesh", "uttar pradesh", "uttarakhand", "uttarakhand", "uttarakhand", "uttarakhand", "west bengal", "west bengal",
+                "west bengal", "west bengal", "west bengal", "west bengal", "west bengal", "west bengal", "west bengal", "west bengal",
+                "west bengal", "west bengal", "west bengal", "west bengal"};
+
+
+
+        double  minDist=Integer.MAX_VALUE;
+        int minIndex=147;
+        for(int i=0;i<146;i++){
+            //Log.i("compare", "findSearchedCity: "+cityArr[i]+city_name);
+            if(cityArr[i].equals(city_name)){
+                minIndex=i;
+            }
+        }
+        if(minIndex<146)
+            return new String[] {stateArr[minIndex],cityArr[minIndex]};
+        else
+            return new String[] {"none","none"};
     }
 
 
